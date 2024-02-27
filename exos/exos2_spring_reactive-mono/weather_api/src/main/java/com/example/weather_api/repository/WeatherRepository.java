2eec607350ae83ecfa27faffaf1894664532888d
Pauline Laout <laout.pauline@hotmail.fr>
2024-02-27 17:22:41 +0100

    update

---
+++ exos/exos2_spring_reactive-mono/weather_api/src/main/java/com/example/weather_api/repository/WeatherRepository.java
@@ -0,0 +1,6 @@
+package com.example.weather_api.repository;
+
+import com.example.weather_api.entity.WeatherForecast;
+
+public class WeatherRepository extends R2dbcRepository<WeatherForecast, Long> {
+}
